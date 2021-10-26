1b226ed058123700d3eb75c9ed9ab99793d08929
Anksus <sankitpramod.min18@itbhu.ac.in>
2021-10-26 22:46:06 +0530

    yo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
 #! /usr/bin/env node
 
 const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const fs = require("fs");
 const argv = yargs(hideBin(process.argv)).argv;
 
-console.log("running");
+const pattern = argv._[0];
+const fileName = argv._[1];
+const param1 = argv._[2];
+
+const logic = (props) => {
+  // to search for key
+  if (param1 == "-k") {
+    console.log(props);
+    const filterKeys = props.filter((item) => {
+      const obj = Object.keys(item);
+      console.log(item, obj);
+      return obj.join("").indexOf(pattern) !== -1;
+    });
+
+    console.log(filterKeys);
+  }
+  // to search for values
+  else if (param1 == "-v") {
+    const filterValues = props.filter((item) => {
+      const obj = Object.values(item);
+      return obj.join("").indexOf(pattern) !== -1;
+    });
+
+    console.log(filterValues);
+  }
+  // case insensitive
+  else if (param1 == "-i") {
+    const filterValues = json_sample_log.filter((item) => {
+      const obj = Object.values(item).concat(Object.keys(item));
+      const lowercased = obj.map((name) => name.toLowerCase());
+      console.log(obj);
+      return lowercased.join("").indexOf(param1Value.toLowerCase()) != -1;
+    });
+    console.log(filterValues);
+  }
+};
+
+const customerFile = fs.readFileSync("./json_sample.log", "utf-8");
+
+if (typeof customerFile === "string") {
+  console.log("yes");
+  const val = JSON.parse(customerFile);
+  console.log(val);
+}
